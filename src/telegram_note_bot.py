ab347ae512ee3698fa0361d97c9e848bb8ed41c2
larsanik <larsanik@gmail.com>
2026-01-27 15:02:40 +0300

    add branch calendar

--- src/telegram_note_bot.py
+++ src/telegram_note_bot.py
@@ -8,13 +8,15 @@ import logging
 import os
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import (
-    Updater,
+    Application,
     CommandHandler,
     MessageHandler,
-    Filters,
+    filters,
     ConversationHandler
 )
-import secrets  # API_TOKEN = '<ТОКЕN>'
+import src.secrets as secrets  # API_TOKEN = '<ТОКЕN>'
+
+import asyncio
 
 # Включение логирования
 logging.basicConfig(
@@ -92,7 +94,7 @@ def cancel(update, context) -> int:
 
 
 # обработчик для команды /start
-def create_start_handler(update, context):
+async def create_start_handler(update, context):
     """Формирование сообщения пользователю по команде start."""
     try:
         msg_start = """ Бот для работы с заметками.
@@ -108,10 +110,10 @@ def create_start_handler(update, context):
         /key_off - выключение виртуальной клавиатуры
         /help - выводит справку по командам
         """
-        context.bot.send_message(chat_id=update.message.chat_id, text=msg_start)
+        await context.bot.send_message(chat_id=update.message.chat_id, text=msg_start)
     except AttributeError as err:
         # Отправить пользователю сообщение об ошибке
-        context.bot.send_message(chat_id=update.message.chat_id, text=f"Произошла ошибка: {err}")
+        await context.bot.send_message(chat_id=update.message.chat_id, text=f"Произошла ошибка: {err}")
 
 
 def read_note(note_name) -> str:
@@ -144,10 +146,10 @@ def get_name_note_read(update, context) -> int:
         logger.error(f'Произошла ошибка: {err}')
 
 
-def create_read_handler(update, context) -> None:
+async def create_read_handler(update, context) -> None:
     """Обработчик для команды read"""
     try:
-        update.message.reply_text('Введите имя заметки для чтения: ')
+        await update.message.reply_text('Введите имя заметки для чтения: ')
         return NAME
     except AttributeError as err:
         # Отправить пользователю сообщение об ошибке
@@ -169,41 +171,41 @@ def edit_note(note_name, note_text) -> str:
         logger.error(f'Произошла ошибка : {err}')
 
 
-def get_name_note_edit(update, context) -> int:
+async def get_name_note_edit(update, context) -> int:
     """Запрос нового текста заметки."""
     try:
         user = update.message.from_user
         logger.info(f"Пользователь:  {user.first_name}. Имя редактируемой заметки: {update.message.text}.")
         context.user_data['note_name'] = update.message.text
         if os.path.isfile(context.user_data['note_name'] + '.txt'):
-            update.message.reply_text('Введите новый текст заметки: ')
+            await update.message.reply_text('Введите новый текст заметки: ')
             return TEXT
         else:
             logger.error(f"Пользователь:  {user.first_name}. Заметка {context.user_data['note_name']} не найдена.")
-            update.message.reply_text(f"Заметка с именем {context.user_data['note_name']} не найдена")
-            update.message.reply_text('Введите имя заметки для редактирования еще раз:')
+            await update.message.reply_text(f"Заметка с именем {context.user_data['note_name']} не найдена")
+            await update.message.reply_text('Введите имя заметки для редактирования еще раз:')
             return NAME
     except AttributeError as err:
         logger.error(f'Произошла ошибка : {err}')
 
 
-def get_text_note_edit(update, context) -> int:
+async def get_text_note_edit(update, context) -> int:
     """Выход из опроса."""
     try:
         user = update.message.from_user
         logger.info(f"Пользователь:  {user.first_name}. Текст редактируемой заметки: {update.message.text}")
         context.user_data['note_text'] = update.message.text
         # создаем заметку и выводим сообщение о результате
-        update.message.reply_text(edit_note(context.user_data['note_name'], context.user_data['note_text']))
+        await update.message.reply_text(edit_note(context.user_data['note_name'], context.user_data['note_text']))
         return ConversationHandler.END
     except AttributeError as err:
         logger.error(f'Произошла ошибка : {err}')
 
 
-def create_edit_handler(update, context) -> None:
+async def create_edit_handler(update, context) -> None:
     """Обработчик для команды edit"""
     try:
-        update.message.reply_text('Введите имя заметки для редактирования: ')
+        await update.message.reply_text('Введите имя заметки для редактирования: ')
         return NAME
     except AttributeError as err:
         # Отправить пользователю сообщение об ошибке
@@ -228,23 +230,23 @@ def delete_note(note_name) -> str:
         logger.error(f'Произошла ошибка: {err}')
 
 
-def get_name_note_delete(update, context) -> int:
+async def get_name_note_delete(update, context) -> int:
     """Запрос имени заметки для удаления."""
     try:
         user = update.message.from_user
         logger.info(f"Пользователь:  {user.first_name}. Имя заметки для удаления : {update.message.text}.")
         context.user_data['note_name'] = update.message.text
         # удаляем заметку и выводим сообщение о результате
-        update.message.reply_text(delete_note(context.user_data['note_name']))
+        await update.message.reply_text(delete_note(context.user_data['note_name']))
         return ConversationHandler.END
     except AttributeError as err:
         logger.error(f'Произошла ошибка: {err}')
 
 
-def create_delete_handler(update, context) -> int:
+async def create_delete_handler(update, context) -> int:
     """Обработчик для команды delete"""
     try:
-        update.message.reply_text('Введите имя заметки для удаления: ')
+        await update.message.reply_text('Введите имя заметки для удаления: ')
         return NAME
     except AttributeError as err:
         # Отправить пользователю сообщение об ошибке
@@ -272,7 +274,7 @@ def display_sorted_notes(update, context) -> None:
         logger.error(f'Произошла ошибка: {err}')
 
 
-def key_on(update, context) -> None:
+async def key_on(update, context) -> None:
     """Добавляет виртуальную клавиатуру с командами"""
     reply_keyboard = [['/start'],
                       ['/create'],
@@ -284,7 +286,7 @@ def key_on(update, context) -> None:
                       ['/key_off'],
                       ['/help']]
 
-    update.message.reply_text(
+    await update.message.reply_text(
         'Виртуальная клавиатура добавлена в бот.',
         reply_markup=ReplyKeyboardMarkup(
             reply_keyboard, resize_keyboard=True, one_time_keyboard=True,
@@ -293,17 +295,17 @@ def key_on(update, context) -> None:
     )
 
 
-def key_off(update, context) -> None:
+async def key_off(update, context) -> None:
     """Убирает виртуальную клавиатуру с командами"""
-    update.message.reply_text(
+    await update.message.reply_text(
         'Виртуальная клавиатура убрана из бота.',
         reply_markup=ReplyKeyboardRemove(),
     )
 
 
-def help_view(update, context) -> None:
+async def help_view(update, context) -> None:
     """Выводит справку по командам"""
-    update.message.reply_text(""" Бот для работы с заметками.
+    await update.message.reply_text(""" Бот для работы с заметками.
         Команды:
         /start - запуск бота
         /create - создание заметки
@@ -323,77 +325,89 @@ def main() -> None:
     """Запуск бота."""
     try:
         # создание обработчика с токеном
-        updater = Updater(token=secrets.API_TOKEN)
+        #updater = Updater(token=secrets.API_TOKEN)
+        application = Application.builder().token(secrets.API_TOKEN).build()
 
         # получение диспетчера
-        dispatcher = updater.dispatcher
+        #dispatcher = updater.dispatcher
 
         # обработка команды /start
-        updater.dispatcher.add_handler(CommandHandler('start', create_start_handler))
+        #updater.dispatcher.add_handler(CommandHandler('start', create_start_handler))
+        application.add_handler(CommandHandler('start', create_start_handler))
 
         # диалог для создания заметки, шаги NAME, ТEXT
         conv_handler_create = ConversationHandler(
             entry_points=[CommandHandler('create', create_note_handler)],
             states={
-                NAME: [MessageHandler(Filters.text & ~Filters.command, get_name_note_create)],
-                TEXT: [MessageHandler(Filters.text & ~Filters.command, get_text_note)],
+                NAME: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_name_note_create)],
+                TEXT: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_text_note)],
             },
             fallbacks=[CommandHandler('cancel', cancel)],  # принудительный выход из диалога по команде /cancel
         )
-        dispatcher.add_handler(conv_handler_create)
+        #dispatcher.add_handler(conv_handler_create)
+        application.add_handler(conv_handler_create)
 
         # диалог для чтения заметки, шаг NAME
         conv_handler_read = ConversationHandler(
             entry_points=[CommandHandler('read', create_read_handler)],
             states={
-                NAME: [MessageHandler(Filters.text & ~Filters.command, get_name_note_read)],
+                NAME: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_name_note_read)],
             },
             fallbacks=[CommandHandler('cancel', cancel)],  # принудительный выход из диалога по команде /cancel
         )
-        dispatcher.add_handler(conv_handler_read)
+        #dispatcher.add_handler(conv_handler_read)
+        application.add_handler(conv_handler_read)
 
         # диалог для редактирования заметки, шаги NAME, ТEXT
         conv_handler_edit = ConversationHandler(
             entry_points=[CommandHandler('edit', create_edit_handler)],
             states={
-                NAME: [MessageHandler(Filters.text & ~Filters.command, get_name_note_edit)],
-                TEXT: [MessageHandler(Filters.text & ~Filters.command, get_text_note_edit)],
+                NAME: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_name_note_edit)],
+                TEXT: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_text_note_edit)],
             },
             fallbacks=[CommandHandler('cancel', cancel)],  # принудительный выход из диалога по команде /cancel
         )
-        dispatcher.add_handler(conv_handler_edit)
+        #dispatcher.add_handler(conv_handler_edit)
+        application.add_handler(conv_handler_edit)
 
         # диалог для удаления заметки, шаг NAME
         conv_handler_delete = ConversationHandler(
             entry_points=[CommandHandler('delete', create_delete_handler)],
             states={
-                NAME: [MessageHandler(Filters.text & ~Filters.command, get_name_note_delete)],
+                NAME: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_name_note_delete)],
             },
             fallbacks=[CommandHandler('cancel', cancel)],  # принудительный выход из диалога по команде /cancel
         )
-        dispatcher.add_handler(conv_handler_delete)
+        #dispatcher.add_handler(conv_handler_delete)
+        application.add_handler(conv_handler_delete)
 
         # обработка команды /display
-        updater.dispatcher.add_handler(CommandHandler('display', display_sorted_notes))
+        #updater.dispatcher.add_handler(CommandHandler('display', display_sorted_notes))
+        application.add_handler(CommandHandler('display', display_sorted_notes))
 
         # обработка команды /key_on
-        updater.dispatcher.add_handler(CommandHandler('key_on', key_on))
+        #updater.dispatcher.add_handler(CommandHandler('key_on', key_on))
+        application.add_handler(CommandHandler('key_on', key_on))
 
         # обработка команды /key_off
-        updater.dispatcher.add_handler(CommandHandler('key_off', key_off))
+        #updater.dispatcher.add_handler(CommandHandler('key_off', key_off))
+        application.add_handler(CommandHandler('key_off', key_off))
 
         # обработка команды /help
-        updater.dispatcher.add_handler(CommandHandler('help', help_view))
+        #updater.dispatcher.add_handler(CommandHandler('help', help_view))
+        application.add_handler(CommandHandler('help', help_view))
 
         # запуск бота
-        updater.start_polling()
+        #updater.start_polling()
+        application.run_polling()
 
         # для корректной остановки бота по запросу из ide
-        updater.idle()
+        #updater.idle()
+        application.idle()
 
     except AttributeError as err:
         logger.error(f'Произошла ошибка: {err}')
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
