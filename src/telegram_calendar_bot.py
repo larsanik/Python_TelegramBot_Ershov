1bdd180fb2d9e37dab1bf76dfa83aea8fcad4076
larsanik <larsanik@gmail.com>
2026-03-12 09:26:41 +0300

    dev def with calendar display event finish

--- src/telegram_calendar_bot.py
+++ src/telegram_calendar_bot.py
@@ -153,26 +153,16 @@ class Calendar:
 
     # метод display_event
     def display_event(self) -> str:
-        # str_out = ''
-        # for el in self.events.items():
-        #     # print(el[1].items())
-        #     for key, val in el[1].items():
-        #         str_out = str_out + str(key) + ': ' + str(val) + ' | '
-        #     str_out = str_out + '\n'
         cursor = self.conn.cursor()
         cursor.execute("""
                     SELECT id, name, date, time, details 
                     FROM events
                     """)
         rows = cursor.fetchall()
-        # rows = [('событие1', datetime.date(2026, 2, 10), datetime.time(15, 16), 'Описание события'), ...] todo убрать
         str_out = ''
-        for id, name, d, t, details in rows: # todo разобрать запись по человечески посмотри как забирает данные read_event
-            # Форматируем дату (ДД.ММ.ГГГГ) и время (ЧЧ:ММ)
-            date_str = d.strftime("%d.%m.%Y")
-            time_str = t.strftime("%H:%M")
-            line = f"[{date_str} {time_str}] {name}: {details}"
-            str_out = str_out + line + "\n"
+        for id_event, name, date, time, details in rows:
+            line = f"Событие номер {id_event} | Наименование: {name}| Дата: {date} | Время: {time} | Детали: {details} \n"
+            str_out = str_out + line
         return str_out
 
     # метод edit_event
